b8ecbc8e79cf680d66978e0cdf6f277c8e74e62f
Olivier Adam <olivier@legalife.fr>
2020-08-11 15:30:28 +0200

    swap arguments to remove parenthese

--- src/Api.elm
+++ src/Api.elm
@@ -9,8 +9,8 @@ type alias Token =
     { demo : Bool, token : String }
 
 
-mockHttpGet : a -> (Result Http.Error a -> msg) -> Cmd msg
-mockHttpGet result toMsg =
+mockHttpGet : (Result Http.Error a -> msg) -> a -> Cmd msg
+mockHttpGet toMsg result =
     Process.sleep 1500
         |> Task.andThen (\_ -> Task.succeed (Ok result))
         |> Task.perform toMsg
@@ -19,13 +19,13 @@ mockHttpGet result toMsg =
 authenticate : ( String, String ) -> (Result Http.Error Token -> msg) -> Cmd msg
 authenticate ( login, password ) toMsg =
     if login == "demo" then
-        mockHttpGet (Token True "mockToken") toMsg
+        mockHttpGet toMsg <| Token True "mockToken"
 
     else
         Http.get
             { url = "/api/login/"
 
-            -- partial application of Token to obtain stringToTokenMapper
+            -- `Token False` is of type String -> Token
             , expect =
                 Result.map (Token False)
                     >> toMsg
