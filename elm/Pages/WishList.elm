5734d1b18eadbf734df85051a2daa5f59411bdef
Olivier Adam <olivier@legalife.fr>
2020-08-26 08:51:45 +0200

    present edition wip

--- elm/Pages/WishList.elm
+++ elm/Pages/WishList.elm
@@ -15,13 +15,17 @@ type Msg
     = GotPresents (Result Http.Error (List Present))
     | ChangeList (Maybe UserId)
     | UpdatePresent Present
+    | EditPresent Present
     | GotUpdatedPresent (Result Http.Error Present)
+    | UpdateEditionTitle String
+    | UpdateEditionDescription String
 
 
 type State
     = Loading
     | Fail
-    | ShowPresents (List Present)
+    | ShowingPresents (List Present)
+    | EditingPresent (List Present) { id : Maybe PresentId, title : String, description : String }
 
 
 type Model
@@ -59,22 +63,44 @@ update msg (Model model) =
             )
 
         GotPresents (Ok presents) ->
-            ( Model { model | state = ShowPresents presents }
+            ( Model { model | state = ShowingPresents presents }
             , Cmd.none
             )
 
+        EditPresent present ->
+            case model.state of
+                ShowingPresents presents ->
+                    ( Model { model | state = EditingPresent presents { id = Just present.id, title = present.title, description = present.description } }
+                    , Cmd.none
+                    )
+
+                _ ->
+                    ( Model model, Cmd.none )
+
         UpdatePresent present ->
-            ( Model { model | state = replacePresent model.state present }
-            , Api.updatePresent model.session.token present GotUpdatedPresent
-            )
+            replacePresentIfLoaded (Model model) present
+
+        UpdateEditionTitle title ->
+            case model.state of
+                EditingPresent list edition ->
+                    ( Model { model | state = EditingPresent list { edition | title = title } }, Cmd.none )
+
+                _ ->
+                    ( Model model, Cmd.none )
+
+        UpdateEditionDescription desc ->
+            case model.state of
+                EditingPresent list edition ->
+                    ( Model { model | state = EditingPresent list { edition | description = desc } }, Cmd.none )
+
+                _ ->
+                    ( Model model, Cmd.none )
 
         GotUpdatedPresent (Err _) ->
             ( Model model, Cmd.none )
 
         GotUpdatedPresent (Ok present) ->
-            ( Model { model | state = replacePresent model.state present }
-            , Cmd.none
-            )
+            replacePresentIfLoaded (Model model) present
 
         ChangeList Nothing ->
             ( Model model, Cmd.none )
@@ -92,24 +118,38 @@ update msg (Model model) =
                     ( Model model, Cmd.none )
 
 
-replacePresent : State -> Present -> State
-replacePresent state present =
-    case state of
-        ShowPresents presents ->
-            ShowPresents
-                (List.map
-                    (\p ->
-                        if p.id == present.id then
-                            present
+replacePresent : Model -> List Present -> Present -> ( Model, Cmd Msg )
+replacePresent (Model model) presents present =
+    ( Model
+        { model
+            | state =
+                ShowingPresents
+                    (List.map
+                        (\p ->
+                            if p.id == present.id then
+                                present
 
-                        else
-                            p
+                            else
+                                p
+                        )
+                        presents
                     )
-                    presents
-                )
+        }
+    , Api.updatePresent model.session.token present GotUpdatedPresent
+    )
+
+
+replacePresentIfLoaded : Model -> Present -> ( Model, Cmd Msg )
+replacePresentIfLoaded (Model model) present =
+    case model.state of
+        ShowingPresents presents ->
+            replacePresent (Model model) presents present
+
+        EditingPresent presents _ ->
+            replacePresent (Model model) presents present
 
         _ ->
-            state
+            ( Model model, Cmd.none )
 
 
 fontawesome : String -> Html Msg
@@ -134,6 +174,7 @@ viewPresent (Model model) present =
                 [ button
                     [ class "btn btn-default"
                     , hidden offered
+                    , onClick <| EditPresent present
                     ]
                     [ fontawesome "pencil", text " modifier" ]
                 , button
@@ -167,8 +208,21 @@ viewPresents (Model model) =
         Fail ->
             [ div [ class "alert alert-danger" ] [ text "une erreur s'est produite sur le serveur" ] ]
 
-        ShowPresents presents ->
+        ShowingPresents presents ->
             List.map (viewPresent (Model model)) presents
+
+        EditingPresent presents edition ->
+            [ form []
+                [ div [ class "form-group" ]
+                    [ label [ attribute "for" "present-title" ] [ text "Titre" ]
+                    , input [ id "present-title", class "form-control", value edition.title, onInput UpdateEditionTitle ] []
+                    ]
+                , div [ class "form-group" ]
+                    [ label [ attribute "for" "present-description" ] [ text "Description" ]
+                    , textarea [ id "present-description", class "form-control", onInput UpdateEditionDescription ] [ text edition.description ]
+                    ]
+                ]
+            ]
 
 
 userOption : Model -> User -> Html Msg
@@ -185,14 +239,41 @@ listSelect (Model model) =
     select [ id "list-picker", class "form-select", onInput (ChangeList << userIdFromString) ] options
 
 
+breadcrumbEditionTitle : String -> String
+breadcrumbEditionTitle title =
+    if String.isEmpty title then
+        "Nouveau cadeau"
+
+    else
+        title
+
+
+breadcrumbEnd : Model -> List (Html Msg)
+breadcrumbEnd (Model model) =
+    case model.state of
+        EditingPresent _ edition ->
+            [ li [ class "breadcrumb-item" ]
+                [ a [ attribute "role" "button" ]
+                    [ text model.user.name ]
+                ]
+            , li [ attribute "aria-current" "page", class "breadcrumb-item" ]
+                [ text (breadcrumbEditionTitle edition.title)
+                ]
+            ]
+
+        _ ->
+            [ li [ attribute "aria-current" "page", class "breadcrumb-item" ]
+                [ listSelect (Model model)
+                ]
+            ]
+
+
 view : Model -> List (Html Msg)
 view (Model model) =
     nav [ attribute "aria-label" "breadcrumb" ]
         [ ol [ class "breadcrumb" ]
-            [ li [ class "breadcrumb-item" ] [ text "Secret Wishlist" ]
-            , li [ attribute "aria-current" "page", class "breadcrumb-item" ]
-                [ listSelect (Model model)
-                ]
-            ]
+            (li [ class "breadcrumb-item" ] [ text "Secret Wishlist" ]
+                :: breadcrumbEnd (Model model)
+            )
         ]
         :: viewPresents (Model model)
